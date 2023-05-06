538eb7e4b462ec2526c5b28227f8f07720e4c41a
DmitryLorents <zmey.task@gmail.com>
2023-05-06 19:16:28 +0600

    Initial Commit

---
+++ CustomTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTabBar
+//
+//  Created by Дмитрий Лоренц on 06.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
